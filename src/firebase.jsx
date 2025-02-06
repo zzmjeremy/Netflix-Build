4d7584f6da351dca5c3e4559365a549ab42c08b1
zzmjeremy <zhuominzhucontact@gmail.com>
2025-02-05 16:00:13 -0800

    Init

---
+++ src/firebase.jsx
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCwaAqM4SAUtYy0KjSaaSw6oN1milemPyw",
+  authDomain: "netflix-clone-a8c08.firebaseapp.com",
+  projectId: "netflix-clone-a8c08",
+  storageBucket: "netflix-clone-a8c08.firebasestorage.app",
+  messagingSenderId: "991993287027",
+  appId: "1:991993287027:web:4bd4a824e8b32bb619f506",
+};
