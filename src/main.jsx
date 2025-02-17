cc5cd537f946f52eb453404a718ad106c44cef81
zzmjeremy <zhuominzhucontact@gmail.com>
2025-02-16 22:59:35 -0800

    Deploy the Build using Firebase

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux' // ✅ 新增
+import { store } from './app/store'    // ✅ 新增
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}> {/* ✅ 包裹 App 组件 */}
+      <App />
+    </Provider>
   </StrictMode>,
 )
