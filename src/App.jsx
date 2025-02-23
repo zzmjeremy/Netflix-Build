d4686908bb039ad778ee71fabd51d1b154fce9fe
zzmjeremy <zhuominzhucontact@gmail.com>
2025-02-22 17:55:46 -0800

    Implemented Redux

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,31 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LoginScreen from "./screens/LoginScreen";
 import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
 function App() {
   const [count, setCount] = useState(0);
-  const user = null;
-  useEffect(() =>{
-    const unsubscribe = auth.onAuthStateChanged(userAuth =>{
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
         //Logged in
-        console.log(userAuth);
-      } else{
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
+      } else {
         //Logged out
+        dispatch(logout);
       }
     });
 
     return unsubscribe;
-  })
+  }, []);
   return (
     <div className="app">
       <Router>
